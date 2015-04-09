9fc7b9e2e9a4b8b9e3527b60d5cc4c138984eb55
hcpzhe <zheyun999@163.com>
2015-04-09 16:14:27 +0800

    奖金记录,提现记录, 增加会员搜索功能--view未完成

--- lexing/Lib/Action/Admin/BonusAction.class.php
+++ lexing/Lib/Action/Admin/BonusAction.class.php
@@ -5,21 +5,26 @@ class BonusAction extends AdminbaseAction {
 	/**
 	 * 升级记录筛选列表
 	 */
-	public function lists() {
+	public function lists($account=null) {
 		$model = New Model('Bonus');
 		$member_M = new Model('Member');
 		
-        $list = $this->_lists($model,null,'create_time desc');
-        $this->assign('list', $list); //记录列表
-        
+		$memeber_map = array(); $bouns_map = array();
+		if(isset($account)){
+			$memeber_map['account'] = $account;
+			$bouns_map['member_id'] = $member_M->where($memeber_map)->getField('id');
+		}
+		$list = $this->_lists($model,$bouns_map,'create_time desc');
+		$this->assign('list', $list); //记录列表
+		
 		$mem_ids = field_unique($list, 'member_id,source_id'); //列表中用到的会员ID
 		$map = array('id'=>array('in',$mem_ids));
 		$memlist = $member_M->where($map)->getField('id,account,realname');
 		$this->assign('memlist',$memlist); //列表用到的会员列表, ID为key索引
-        
-        // 记录当前列表页的cookie
-        cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
-        $this->display();
+		
+		// 记录当前列表页的cookie
+		cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
+		$this->display();
 	}
 	
 }
--- lexing/Lib/Action/Admin/CashAction.class.php
+++ lexing/Lib/Action/Admin/CashAction.class.php
@@ -15,27 +15,33 @@ class CashAction extends AdminbaseAction {
 	/**
 	 * 记录筛选列表
 	 */
-	public function lists($status=1) {
-		$model = New Model('Cash');
+	public function lists($status=1,$account=null) {
+		$model = New Model('Cash'); $member_M = New Model('Member');
 		//查询条件
 		$map['status'] = $status;
 		
-        $id = (int)I('param.id');
-        if ($id>0) $map['id'] = $id;
-        $list = $this->_lists($model,$map);
-        
+		$id = (int)I('param.id');
+		if ($id>0) {
+			$map['id'] = $id;
+		}elseif (isset($account)) {
+			$memmap = array();
+			$memmap['account']   =   array('like', '%'.$account.'%');
+			$map['member_id'] = $member_M->where($memmap)->getField('id');
+			if (empty($map['member_id'])) $this->error('找不到用户',cookie(C('CURRENT_URL_NAME')));
+		}
+		$list = $this->_lists($model,$map);
+		
 		$mem_ids = field_unique($list, 'member_id'); //列表中用到的会员ID
 		$map = array('id'=>array('in',$mem_ids));
-		$member_M = New Model('Member');
 		$memlist = $member_M->where($map)->getField('id,account,realname');
 		
-        $this->assign('list', $list); //记录列表
-        $this->assign('status', $map['status']); //用于筛选条件的显示
+		$this->assign('list', $list); //记录列表
+		$this->assign('status', $map['status']); //用于筛选条件的显示
 		$this->assign('memlist',$memlist); //列表用到的会员列表, ID为key索引
-        
-        // 记录当前列表页的cookie
-        cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
-        $this->display();
+		
+		// 记录当前列表页的cookie
+		cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
+		$this->display();
 	}
 	
 	/**
@@ -55,8 +61,8 @@ class CashAction extends AdminbaseAction {
 		$this->assign('info',$info);
 		$this->assign('meminfo',$meminfo);
 		
-        // 记录当前列表页的cookie
-        cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
+		// 记录当前列表页的cookie
+		cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
 		$this->display();
 	}
 	
